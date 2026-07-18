52fb44e899bed66c6da02fe57bdad65c9389112b
tsubasa1116 <tsubasa.maeno116@gmail.com>
2026-07-18 23:42:43 +0900

    result

--- Assets/UI_Result/GameResultManager.cs
+++ Assets/UI_Result/GameResultManager.cs
@@ -2,23 +2,28 @@ using UnityEngine;
 
 public static class GameResultManager
 {
-    // インゲームからこの変数に値を代入してシーンを切り替える
-    public static int DestroyCount { get; set; } = 0;
-    public static int ClearTimeSeconds { get; set; } = 0;
+	// インゲームからこの変数に値を代入してシーンを切り替える
+	public static int DestroyCount { get; set; } = 0;
+	public static int ClearTimeSeconds { get; set; } = 0;
 
-    public static void SetResultData(int destroyCount, int clearTimeSeconds)
-    {
-        DestroyCount = destroyCount;
-        ClearTimeSeconds = clearTimeSeconds;
-    }
+	public static void SetResultData(int destroyCount, int clearTimeSeconds)
+	{
+		DestroyCount = destroyCount;
+		ClearTimeSeconds = clearTimeSeconds;
+	}
 
-    public static int CalculateScoreRank()
-    {
-        int basePoint = (DestroyCount * 100) - ClearTimeSeconds;
+	public static int CalculateScore()
+	{
+		return 5000 - (ClearTimeSeconds * 10) + (DestroyCount * 50);
+	}
 
-        if (basePoint >= 1000) return 3; // Sランク
-        if (basePoint >= 500) return 2; // Aランク
-        if (basePoint >= 0) return 1; // Bランク
-        return 0;                        // Cランク
-    }
+	public static int CalculateScoreRank()
+	{
+		int score = CalculateScore();
+
+		if (score >= 3700) return 3; // S
+		if (score >= 3200) return 2; // A
+		if (score >= 2600) return 1; // B
+		return 0;                    // C
+	}
 }
